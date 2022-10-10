ca28dafb0b38b3260b13ca8e6e926d48449c40ab
jhonatan <201800610I@utea.edu.pe>
2022-10-10 11:15:03 -0500

    version 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
-
-
 import 'package:flutter/material.dart';
 
- void main() {
+void main() {
   runApp(
     MyApp(),
   );
-  
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp((super.key));
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
- 
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+        title: Text("DiceApp"),
+        centerTitle: true,
         backgroundColor: Colors.deepPurple,
-        appBar: AppBar(
-          title: Text("DiceApp"),
-          centerTitle: true,
-          backgroundColor: Colors.deepPurple,
-          elevation: 10.0,
-        ),
+        elevation: 10.0,
       ),
     );
   }
